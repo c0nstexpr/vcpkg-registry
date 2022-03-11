a423854ea2bfb18ccd3e9563854607f3a7905814
Blurring Shadow <976999484@qq.com>
2022-03-11 17:06:56 +0800

    Initialization

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# North Wind Traders' vcpkg registry
+# Blurring Shadow's vcpkg registry
 
-This is the vcpkg registry for North Wind Traders,
-a made-up example company for a blog post about vcpkg's
-new registries feature.
+This is the vcpkg registry for my C++ lib.
--- ports/beicode/portfile.cmake
+++ ports/beicode/portfile.cmake
@@ -1,22 +1,18 @@
-vcpkg_from_github(
+vcpkg_from_git(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO northwindtraders/beicode
-	REF 19a1f95c2f56a27ced90227b5e2754a602a08e69
-	SHA512
-7b2bb7acb2a8ff07bff59cfa27247a7b2cced03828919cd65cc0c8cf1f724f5f1e947ed6992dcdbc913fb470694a52613d1861eaaadbf8903e94eb9cdfe4d000
-	HEAD_REF main
+	URL https://github.com/BlurringShadow/stdsharp
+	REF da78c4a26e63b21b5e43ce1424c56ab29ae4fba4
 )
 
-vcpkg_configure_cmake(
-	SOURCE_PATH "${SOURCE_PATH}"
-	PREFER_NINJA
-)
+vcpkg_configure_cmake(SOURCE_PATH "${SOURCE_PATH}" PREFER_NINJA)
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/.github")
 
 file(
 	INSTALL "${SOURCE_PATH}/LICENSE"
 	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-	RENAME copyright)
+	RENAME copyright
+)
