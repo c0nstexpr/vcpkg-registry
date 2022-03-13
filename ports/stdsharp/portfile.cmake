b9dea91521dad04320eb72c7508eb1f58d1584d4
Blurring Shadow <976999484@qq.com>
2022-03-13 22:45:55 +0800

    update

--- ports/stdsharp/portfile.cmake
+++ ports/stdsharp/portfile.cmake
@@ -1,15 +1,16 @@
 vcpkg_from_git(
 	OUT_SOURCE_PATH SOURCE_PATH
 	URL https://github.com/BlurringShadow/stdsharp
-	REF da78c4a26e63b21b5e43ce1424c56ab29ae4fba4
+	REF fe528c72c23fc81550fc690369869d8309104068
 )
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/.github")
 
 vcpkg_configure_cmake(SOURCE_PATH "${SOURCE_PATH}" PREFER_NINJA)
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/.github")
 
 file(
 	INSTALL "${SOURCE_PATH}/LICENSE"
