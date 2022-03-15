66f47ba3c8ad43f18fd9011025657aa6df2967f1
Blurring Shadow <976999484@qq.com>
2022-03-15 23:12:12 +0800

    update

--- ports/boost-ext-ut/portfile.cmake
+++ ports/boost-ext-ut/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO boost-ext/ut
     REF v1.1.9
     HEAD_REF master
-    SHA512 0
+    SHA512 81a6b80948d3a203534244f62f5f3ac57593083cc0c32484498a7d01d29455f7dcb33e2ec0587609b8dff33a81a5551796d7681d48fd93e817d6d0c31697234e
 )
 
 vcpkg_download_distfile(LICENSE_FILE
