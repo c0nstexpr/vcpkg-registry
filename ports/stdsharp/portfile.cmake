985c54b9ba223a6173f1b299fc4bbf01486b5a24
残象之形 <976999484@qq.com>
2022-05-17 14:40:43 +0800

    update

--- ports/stdsharp/portfile.cmake
+++ ports/stdsharp/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/.github")
 
-foreach(path IN $ENV{PATH})
+foreach(path IN LIST $ENV{PATH})
     vcpkg_add_to_path("${path}")
 endforeach()
 
