0ef620b9904946ebc8595c9bf6b26d63edc3d136
残象之形 <976999484@qq.com>
2022-05-18 17:04:00 +0800

    update port

--- ports/stdsharp/portfile.cmake
+++ ports/stdsharp/portfile.cmake
@@ -1,10 +1,10 @@
-set(ver 0.2.4)
+set(ver 0.2.5)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BlurringShadow/stdsharp
     REF ${ver}
-    SHA512 ff88a4476f0a462123138eca8c561480a575a4a98cb7b96f6792868d259a6fbc77f8b6fec2e28459a2f6111f3b9cb3c0b0cc74ffb30ecd2cd8b822ed60ce2e58
+    SHA512 e8ac62020fb20bd2d58db3f10d0627b859d23f8ff697993d9c2fb677de1bbb5c77bca683b49a43692ba434e07fb611a26cefa5b18dc843eb8e5226238b1a9436
     HEAD_REF main
 )
 
@@ -12,7 +12,12 @@ vcpkg_find_acquire_program(GIT)
 get_filename_component(GIT_DIR "${GIT}" DIRECTORY)
 vcpkg_add_to_path("${GIT_DIR}")
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS "-DSTDSHARP_BUILD_TEST=OFF")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DSTDSHARP_BUILD_TEST=OFF
+        -DSTDSHARP_INCLUDE_AS_SYSTEM=OFF
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/stdsharp-${ver})
 
