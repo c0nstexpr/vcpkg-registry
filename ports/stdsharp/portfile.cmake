e228062617f76b9666193b8caac6940afc77a325
残象之形 <976999484@qq.com>
2022-05-18 14:47:01 +0800

    update port

--- ports/stdsharp/portfile.cmake
+++ ports/stdsharp/portfile.cmake
@@ -1,8 +1,10 @@
+set(ver 0.2.3)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BlurringShadow/stdsharp
-    REF 0.2.2
-    SHA512 87cfa41801a9e78ec2fc24fd173293cafde9a99968a9f38168bc8aab92d312ebe75a6d41b31ca14d3a48992efc484151f3a3cb14b0892008b2c3f5789c1db229
+    REF ${ver}
+    SHA512 09da7ac43655af72df666231ba62a6cb83c1cc1189b77c92c3d1cba53ff4fba64ef37505e6237508097fca0f6fb0167d158a2785f51888e6ebfdef55080c9a59
     HEAD_REF main
 )
 
@@ -14,7 +16,7 @@ vcpkg_add_to_path("${GIT_DIR}")
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" PREFER_NINJA OPTIONS "-DSTDSHARP_BUILD_TEST=OFF")
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/stdsharp-0.2.2)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/stdsharp-${ver})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
