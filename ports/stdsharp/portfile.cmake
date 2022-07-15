c805e71fa04c2b3e913e24a42da420217eaec3a2
残象之形 <976999484@qq.com>
2022-07-15 11:22:45 +0800

    update ports

--- ports/stdsharp/portfile.cmake
+++ ports/stdsharp/portfile.cmake
@@ -1,10 +1,10 @@
-set(ver 0.2.7.1)
+set(ver 0.2.7.2)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BlurringShadow/stdsharp
     REF ${ver}
-    SHA512 f0dac24015227ff39fb11ce811a12416aa6ca712536267a08570b42735fc70291f8d9c4accbce0595b84facec729c67d7efe72432f71e32e124fc920fd4a7ae9
+    SHA512 119febbda9b03472cb488e4da5de36a7c523c2626a3d7220a56eac6010623427e2411e7fd65b092d03d77ae6f71a567bce766073d01178b5894983bd078e04ec
     HEAD_REF main
 )
 
