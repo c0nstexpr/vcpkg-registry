095f5085f0bfa9594f465b58f652682c39edb34f
残象之形 <976999484@qq.com>
2022-12-06 16:57:02 +0800

    update port

--- ports/foonathan-memory/portfile.cmake
+++ ports/foonathan-memory/portfile.cmake
@@ -15,10 +15,67 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DFOONATHAN_MEMORY_BUILD_EXAMPLES=OFF
   -DFOONATHAN_MEMORY_BUILD_TESTS=OFF)
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/foonathan-memory-${ver})
 
-file(
-  INSTALL "${SOURCE_PATH}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright)
+vcpkg_cmake_install()
+
+if(EXISTS ${CURRENT_PACKAGES_DIR}/cmake)
+  vcpkg_cmake_config_fixup(CONFIG_PATH cmake TARGET_PATH share/foonathan_memory)
+elseif(EXISTS ${CURRENT_PACKAGES_DIR}/share/foonathan_memory/cmake)
+  vcpkg_cmake_config_fixup(CONFIG_PATH share/foonathan_memory/cmake TARGET_PATH
+                           share/foonathan_memory)
+endif()
+
+vcpkg_copy_pdbs()
+
+# Place header files into the right folders The original layout is not a problem
+# for CMake-based project.
+file(COPY ${CURRENT_PACKAGES_DIR}/include/foonathan_memory/foonathan
+     DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(COPY ${CURRENT_PACKAGES_DIR}/include/foonathan_memory/config_impl.hpp
+     DESTINATION ${CURRENT_PACKAGES_DIR}/include/foonathan/memory)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/foonathan_memory)
+vcpkg_replace_string(
+  ${CURRENT_PACKAGES_DIR}/share/foonathan_memory/foonathan_memory-config.cmake
+  "\${_IMPORT_PREFIX}/include/foonathan_memory" "\${_IMPORT_PREFIX}/include")
+# Place header files into the right folders - Done!
+
+# The Debug version of this lib is built with: #define
+# FOONATHAN_MEMORY_DEBUG_FILL 1 and Release version is built with: #define
+# FOONATHAN_MEMORY_DEBUG_FILL 0 We only have the Release version header files
+# installed, however.
+vcpkg_replace_string(
+  ${CURRENT_PACKAGES_DIR}/include/foonathan/memory/detail/debug_helpers.hpp
+  "#if FOONATHAN_MEMORY_DEBUG_FILL"
+  "#ifndef NDEBUG //#if FOONATHAN_MEMORY_DEBUG_FILL")
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
+     ${CURRENT_PACKAGES_DIR}/debug/share)
+
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/LICENSE
+     ${CURRENT_PACKAGES_DIR}/debug/README.md ${CURRENT_PACKAGES_DIR}/LICENSE
+     ${CURRENT_PACKAGES_DIR}/README.md)
+
+if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL
+                                  "WindowsStore")
+  set(EXECUTABLE_SUFFIX ".exe")
+else()
+  set(EXECUTABLE_SUFFIX "")
+endif()
+
+if(EXISTS ${CURRENT_PACKAGES_DIR}/bin/nodesize_dbg${EXECUTABLE_SUFFIX})
+  file(COPY ${CURRENT_PACKAGES_DIR}/bin/nodesize_dbg${EXECUTABLE_SUFFIX}
+       DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+  vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
+
+  if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin
+         ${CURRENT_PACKAGES_DIR}/debug/bin)
+  else()
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/nodesize_dbg${EXECUTABLE_SUFFIX}
+         ${CURRENT_PACKAGES_DIR}/debug/bin/nodesize_dbg${EXECUTABLE_SUFFIX})
+  endif()
+endif()
+
+# Handle copyright
+configure_file(${SOURCE_PATH}/LICENSE
+               ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
