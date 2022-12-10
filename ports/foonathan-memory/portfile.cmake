cbfa0f7871dc53d44579d524d9e72c2108bfaa79
Blurring Shadow <976999484@qq.com>
2022-12-11 00:02:11 +0800

    update port

--- ports/foonathan-memory/portfile.cmake
+++ ports/foonathan-memory/portfile.cmake
@@ -1,12 +1,10 @@
-set(ver v0.7-2)
-
 vcpkg_from_github(
   OUT_SOURCE_PATH
   SOURCE_PATH
   REPO
   foonathan/memory
   REF
-  ${ver}
+  "v0.7-2"
   SHA512
   e84a567a832138f477997d7b4cbd827a82dfd5d9de8dc0d2833995366253501155663c7d73407ac43a7fd58d42774ddb3582c557ca12800316a732ac7ccab823
   HEAD_REF
@@ -18,22 +16,23 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME foonathan_memory)
+vcpkg_cmake_config_fixup(PACKAGE_NAME foonathan_memory CONFIG_PATH
+                         lib/foonathan_memory/cmake)
 
 vcpkg_copy_pdbs()
 
+vcpkg_copy_tools(TOOL_NAMES nodesize_dbg AUTO_CLEAN)
+
+# Clean out unneeded directories and files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/foonathan_memory")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/foonathan_memory")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/foonathan_memory")
 
-file(COPY "${CURRENT_PACKAGES_DIR}/include/foonathan_memory/foonathan" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-
+# Correct headers path
+file(COPY "${CURRENT_PACKAGES_DIR}/include/foonathan_memory/foonathan"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/foonathan_memory")
-
-vcpkg_copy_tools(TOOL_NAMES nodesize_dbg AUTO_CLEAN)
 
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
