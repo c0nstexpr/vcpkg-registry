2ff7d3689f2a05ec7f5a76d961cecf108a17e090
残象之形 <976999484@qq.com>
2022-12-12 16:42:41 +0800

    update port

--- ports/foonathan-memory/portfile.cmake
+++ ports/foonathan-memory/portfile.cmake
@@ -4,9 +4,9 @@ vcpkg_from_github(
   REPO
   foonathan/memory
   REF
-  "v0.7-2"
+  62a41ddcf919d7066436a2e86cd3567337458e8b
   SHA512
-  e84a567a832138f477997d7b4cbd827a82dfd5d9de8dc0d2833995366253501155663c7d73407ac43a7fd58d42774ddb3582c557ca12800316a732ac7ccab823
+  ba2aceacb8d6637d967b2431180a8cd0e13f5ca846df0ef9a33332a65947a8ab239f94a2dc12cc2b6523a786759a203a3c656ffe365c82e378ba81a4ffc06d54
   HEAD_REF
   main)
 
@@ -15,19 +15,17 @@ vcpkg_cmake_configure(
   -DFOONATHAN_MEMORY_BUILD_TESTS=OFF)
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME foonathan_memory CONFIG_PATH
-                         lib/foonathan_memory/cmake)
+if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/foonathan_memory/cmake)
+  vcpkg_cmake_config_fixup(PACKAGE_NAME foonathan_memory CONFIG_PATH
+                           lib/foonathan_memory/cmake)
+else()
+  vcpkg_cmake_config_fixup(PACKAGE_NAME foonathan_memory CONFIG_PATH
+                           share/foonathan_memory/cmake)
+endif()
 
 vcpkg_copy_pdbs()
 
 vcpkg_copy_tools(TOOL_NAMES nodesize_dbg AUTO_CLEAN)
-
-# Clean out unneeded directories and files
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/foonathan_memory")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/foonathan_memory")
 
 # Correct headers path
 file(COPY "${CURRENT_PACKAGES_DIR}/include/foonathan_memory/foonathan"
@@ -36,3 +34,16 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/foonathan_memory")
 
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# Clean out unneeded directories and files
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if(EXISTS ${CURRENT_PACKAGES_DIR}/debug/lib/foonathan_memory)
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/foonathan_memory")
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/foonathan_memory)
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/foonathan_memory")
+endif()
