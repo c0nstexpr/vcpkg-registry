fe9ac003faf66c513e7ae4c8a78f8fc471f05604
Blurring Shadow <976999484@qq.com>
2023-10-11 15:47:54 +0800

    update port

--- ports/stdsharp/portfile.cmake
+++ ports/stdsharp/portfile.cmake
@@ -1,4 +1,4 @@
-set(ver 0.6.4)
+set(ver 0.7.0)
 
 set(repo_name stdsharp)
 
@@ -10,7 +10,7 @@ vcpkg_from_github(
   REF
   ${ver}
   SHA512
-  fb66f87d68fed47cb49045ccde0b7f24cea5bc2ccd36c167d5d47352bc25076db63a8063fda688c91cb14ee50877bb8c9e2a37c8d29e582813307d8239dbd984
+  d309b032655e7c3b2380e63fb06f9664a8c838987597484c6445bf74fc90d5c500dcba92a6a3f38e22d7a654ba92bf0e582753a4d4f3e5a0ce662a234c2a14c2
   HEAD_REF
   main)
 
